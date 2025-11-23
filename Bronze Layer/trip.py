d7eff692a57fa3acb4b946c4fd38d945c86aab69
Halah Abousohh <eng.halah.mohammed212@outlook.sa>
2025-11-23 16:03:21 +0100

    Delete Bronze Layer/trip.py

--- Bronze Layer/trip.py
+++
@@ -1,9 +0,0 @@
-
-column_names=['trip_id','rideable_id','started_at','ended_at','start_station_id','end_station_id','rider_id']
-df=spark.read.format('csv').option('inferSchema','false').option('header','false').load('dbfs:/FileStore/trip.csv').toDF(*column_names)
-
-df.write.format('delta').option('mergeSchema','true').mode('overwrite').save('/FileStore/delta_trip')
-df.display()
-
-df=spark.read.format('delta').option('header', 'true').load('/FileStore/delta_trip')
-df.write.format('delta').saveAsTable('trip_table')
